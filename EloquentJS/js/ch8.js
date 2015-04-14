6cbf910ae69d8f62f9442463c81a941a569634d7
skylurker <skaldsandshadows@mail.com>
2015-04-14 22:02:07 +0400

    EloqJS, ch8, World turn, letAct and checkDestination

--- EloquentJS/js/ch8.js
+++ EloquentJS/js/ch8.js
@@ -105,6 +105,33 @@ World.prototype.toString = function(){
 	}
 	return output;
 };
+World.prototype.turn = function(){
+	var acted = [];
+	this.grid.forEach(function(critter, vector){
+		if (critter.act && acted.indexOf(critter) == -1){
+			acted.push(critter);
+			this.letAct(critter, vector);
+		}
+	}, this);
+};
+World.prototype.letAct = function(critter, vector){
+	var action = critter.act(new View(this, vector));
+	if (action && action.type == "move"){
+		var dest = this.checkDestination(action, vector);
+		if (dest && this.grid.get(dest) == null){
+			this.grid.set(vector, null);
+			this.grid.set(dest, critter);
+		}
+	}
+};
+World.prototype.checkDestination = function(action, vector){
+	if (directions.hasOwnProperty(action.direction)){
+		var dest = vector.plus(directions[action.direction]);
+		if (this.grid.isInside(dest))
+			return dest;
+	}
+};
+
 function Wall(){};
 // Tutorial: A wall is a simple object—it is used only for taking up space and has no act method.
 
