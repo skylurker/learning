b918773ef6a1774ee44c0cb41ccc228435dfc70a
skylurker <skaldsandshadows@mail.com>
2015-04-14 22:13:38 +0400

    EloqJS, ch8, View, look, findAll, find

--- EloquentJS/js/ch8.js
+++ EloquentJS/js/ch8.js
@@ -132,6 +132,30 @@ World.prototype.checkDestination = function(action, vector){
 	}
 };
 
+function View(world, vector){
+	this.world = world;
+	this.vector = vector;
+}
+View.prototype.look = function(dir){
+	var target = this.vector.plus(directions[dir]);
+	if (this.world.grid.isInside(target))
+		return charFromElement(this.world.grid.get(target));
+	else
+		return "#";
+};
+View.prototype.findAll = function(ch){
+	var found = [];
+	for (var dir in directions)
+		if (this.look(dir) == ch)
+			found.push[dir];
+	return found;
+};
+View.prototype.find = function(ch){
+	var found = this.findAll(ch);
+	if (found.length == 0) return null;
+	return randomElement(found);
+};
+
 function Wall(){};
 // Tutorial: A wall is a simple object—it is used only for taking up space and has no act method.
 
