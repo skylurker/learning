511f6876a7c32a0eaf40fe7076b595a3d0368222
skylurker <skaldsandshadows@mail.com>
2015-04-20 22:13:54 +0400

    EloqJS, ch5, modified tutorial answer by setting a default -1 step

--- EloquentJS/js/ch5.js
+++ EloquentJS/js/ch5.js
@@ -67,13 +67,33 @@ function range2(start, end, step){
 	}
 	return arr;
 }
+/* Slightly modified the tutorial solution so
+	it sets -1 as a default step if start > end */
+function range3(start, end, step){
+	if (step==null && start <= end) step =  1;
+	if (step==null && start > end)  step = -1;
+	var array = [];
+	if (step>0){
+		for (var i=start; i<=end; i+=step){
+			array.push(i);
+		}
+	} else{
+		for (var i=start; i>=end; i+=step){
+			array.push(i);
+		}
+	}
+	return array;
+}
 console.log("Range");
 console.log(range(1, 10));
 console.log(sum(range(1, 10)));
 console.log(range2(1, 10, 2));
 console.log(range2(5, 2, -1));
 console.log(range2(5, 5));
-
+console.log(range3(1, 10, 2));
+console.log(range3(5, 2, -1));
+console.log(range3(5, 5));
+console.log(range3(5, 2));
 
 /* Reversing an array
 
