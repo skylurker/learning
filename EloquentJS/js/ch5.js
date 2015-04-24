34a963e25d24c69eafe7f8d5283d60e48563f95a
skylurker <skaldsandshadows@mail.com>
2015-04-24 22:05:27 +0400

    EloqJS, ch5, reverseArray answers

--- EloquentJS/js/ch5.js
+++ EloquentJS/js/ch5.js
@@ -145,7 +145,16 @@ function reverseArrayInPlace (array){
 		array[i] = tmp;
 	}
 }
-
+/* Answers
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+*/
 var arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
