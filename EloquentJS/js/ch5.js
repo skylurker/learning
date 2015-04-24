0e8b4ef989f46e1f981c068d812d105297f1f5ea
skylurker <skaldsandshadows@mail.com>
2015-04-24 22:02:42 +0400

    EloqJS, ch5, reverseArrayInPlace

--- EloquentJS/js/ch5.js
+++ EloquentJS/js/ch5.js
@@ -123,6 +123,37 @@ console.log("Reverse array");
 console.log(reverseArray(["A", "B", "C"]));
 console.log(reverseArray2(["A", "B", "C"]));
 
+/* The trick is to swap the first and last 
+elements, then the second and second-to-last, 
+and so on. You can do this by looping over 
+half the length of the array (use Math.floor 
+to round down—you don’t need to touch the 
+middle element in an array with an odd length) 
+and swapping the element at position i with 
+the one at position array.length - 1 - i. 
+You can use a local variable to briefly hold 
+on to one of the elements, overwrite that one 
+with its mirror image, and then put the value 
+from the local variable in the place where 
+the mirror image used to be.
+*/
+function reverseArrayInPlace (array){
+	var tmp = 0;
+	for (var i = 0; i<Math.floor(array.length/2); i++){
+		tmp = array[array.length - 1 - i];
+		array[array.length - 1 - i] = array[i]
+		array[i] = tmp;
+	}
+}
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+var arrayValue2 = [1, 2, 3, 4];
+reverseArrayInPlace(arrayValue2);
+console.log(arrayValue2);
+
 /*Deep comparison
 
 The == operator compares objects by identity. 
