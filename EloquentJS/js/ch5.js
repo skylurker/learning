a0b21b94048dc1940480940160e2d56f5bbae8d1
skylurker <skaldsandshadows@mail.com>
2015-04-25 19:47:09 +0400

    EloqJS, ch5, arrayToList, doesn't work properly (makes a giantlist)

--- EloquentJS/js/ch5.js
+++ EloquentJS/js/ch5.js
@@ -163,6 +163,79 @@ var arrayValue2 = [1, 2, 3, 4];
 reverseArrayInPlace(arrayValue2);
 console.log(arrayValue2);
 
+/* A list
+
+Objects, as generic blobs of values, 
+can be used to build all sorts of data structures. 
+A common data structure is the list (not to be confused 
+with the array). A list is a nested set of objects, 
+with the first object holding a reference to the second, 
+the second to the third, and so on.
+
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+
+A nice thing about lists is that they can share 
+parts of their structure. For example, if I create 
+two new values {value: 0, rest: list} and {value: -1, rest: list} 
+(with list referring to the variable defined earlier), 
+they are both independent lists, but they share 
+the structure that makes up their last three elements. 
+In addition, the original list is also 
+still a valid three-element list.
+
+Write a function arrayToList that builds up 
+a data structure like the previous one when 
+given [1, 2, 3] as argument, and write a listToArray 
+function that produces an array from a list. 
+Also write the helper functions prepend, which 
+takes an element and a list and creates a new list 
+that adds the element to the front of the input list, 
+and nth, which takes a list and a number and returns 
+the element at the given position in the list, 
+or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth.
+*/
+
+/* Hints
+Building up a list is best done back to front. 
+So arrayToList could iterate over the array 
+backward (see previous exercise) and, for each 
+element, add an object to the list. You can use 
+a local variable to hold the part of the list that 
+was built so far and use a pattern 
+like list = {value: X, rest: list} to add an element.
+*/
+
+function arrayToList(array){
+	var list = {value: null, rest: null}
+	var tmp = list;
+	for(var i=array.length-1; i>=0; i--){
+		list.rest = tmp;
+		list.value = array[i];
+		tmp = list;
+	}
+
+	return list;
+}
+
+
+console.log("Array to list");
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+
+
 /*Deep comparison
 
 The == operator compares objects by identity. 
