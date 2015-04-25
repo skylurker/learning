ef298c10d9643fe30b6dd55eb9cc991709f87c23
skylurker <skaldsandshadows@mail.com>
2015-04-25 20:13:32 +0400

    EloqJS, ch5, prepend; TODO: recursive nth

--- EloquentJS/js/ch5.js
+++ EloquentJS/js/ch5.js
@@ -279,6 +279,18 @@ console.log("nth");
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
 
+/*
+The recursive version of nth will, similarly, look at an ever smaller part of the “tail” of the list and at the same time count down the index until it reaches zero, at which point it can return the value property of the node it is looking at. To get the zeroeth element of a list, you simply take the value property of its head node. To get element N + 1, you take the Nth element of the list that’s in this list’s rest property.
+*/
+
+function prepend(value, list){
+	var list = {value: value, rest: list};
+	return list;
+}
+console.log("prepend");
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+
 /*Deep comparison
 
 The == operator compares objects by identity. 
