6539b8e2028ebd98ead6e2e0f871e1b85f539d52
skylurker <skaldsandshadows@mail.com>
2015-05-01 01:28:02 +0400

    Reddit, 203 The Start of Something Big, a super lazy solution

---
+++ dailyprogrammer/203_The_Start_of_Something_Big.js
@@ -0,0 +1,26 @@
+/* 
+[2015-2-23] Challenge #203 [Easy] The Start of Something Big
+Description
+
+All great things start with something small. Sometimes people don't even realise what goes into making a 'small' thing.
+
+A popular story is linked above about a group of graphics programmers who create a rendering engine in some amount of time. After some time HR came to see what the programmers had accomplished. They responded by showing a black triangle on a tv.
+
+HR was less than impressed (understandle for a non techie) but it goes to show the natural evolution of a program. What they didn't realise is that the programmers have created their base engine and can now easily add and extend on top of it.
+
+Maybe you can follow similar steps?
+Challenge
+
+On your screen, display a square.
+
+You may use any libraries available to you.
+
+The square may be of any size and of any colour.
+
+*/
+
+
+/* */
+
+
+console.log("a square".toUpperCase());
