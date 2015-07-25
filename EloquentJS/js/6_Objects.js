9a74e37271acd095a00df0409b0ff3049ff9c98a
skylurker <skaldsandshadows@mail.com>
2015-07-25 13:45:15 +0300

    EloqJS: Objects, Another cell; it works; didn't check the answers' code

--- EloquentJS/js/6_Objects.js
+++ EloquentJS/js/6_Objects.js
@@ -17,7 +17,57 @@ Vector.prototype.minus = function(vector2) {
 Object.defineProperty(Vector.prototype, "length", {
   get: function() {return Math.sqrt(this.x * this.x + this.y * this.y);}
 });
-
+console.log("Vector type");
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 console.log(new Vector(3, 4).length);
+
+/* Another cell */
+function repeat(string, times) {
+  var result = "";
+  for (var i = 0; i < times; i++)
+    result += string;
+  return result;
+};
+function TextCell(text) {
+  this.text = text.split("\n");
+};
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+
+
+
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+};
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.inner.minWidth(), this.width);
+};
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.inner.minHeight(), this.height);
+};
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height);
+};
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log("Another cell");
+console.log(sc.minWidth());
+console.log(sc.minHeight());
+console.log(sc.draw(3, 2));
