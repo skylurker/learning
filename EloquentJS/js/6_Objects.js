8b8e81533ef9a0b4f2d60071f70d2750cd5b599b
skylurker <skaldsandshadows@mail.com>
2015-07-25 13:32:20 +0300

    EloqJS: Objects, Vector type; it works; didn't check the answers' code

--- EloquentJS/js/6_Objects.js
+++ EloquentJS/js/6_Objects.js
@@ -1,1 +1,23 @@
- 
+/* Eloquent Java Script */
+/* Objects */
+
+/* Vector type */
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Vector.prototype.plus = function(vector2) {
+  return new Vector(this.x + vector2.x, this.y + vector2.y);
+};
+Vector.prototype.minus = function(vector2) {
+  return new Vector(this.x - vector2.x, this.y - vector2.y);
+};
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {return Math.sqrt(this.x * this.x + this.y * this.y);}
+});
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(3, 4).length);
