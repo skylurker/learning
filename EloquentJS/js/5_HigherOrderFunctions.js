d1e2cdaf1b434192425f4e1e2c1bdc8e94763455
skylurker <skaldsandshadows@mail.com>
2015-07-25 13:23:02 +0300

    EloqJS: Higher Order Functions, Mother-child age difference; didn't check if it works, since I need the ANCESTRY file

--- EloquentJS/js/5_HigherOrderFunctions.js
+++ EloquentJS/js/5_HigherOrderFunctions.js
@@ -1,6 +1,12 @@
 /* Eloquent Java Script */
 /* Higher Order Functions */
 
+var ancestry = JSON.parse(ANCESTRY_FILE);
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
 /* Flattening */
 var input = [[1, 2, 3],
 	     [4, 5],
@@ -8,3 +14,19 @@ var input = [[1, 2, 3],
 console.log(input.reduce(function (arrayA, arrayB) {
 			  return arrayA.concat(arrayB);
 }));
+
+/* Mother-child age difference */
+
+// Not everyone has an object with their mother. Let's filter them out:
+//var hasMother = ancestry.filter(function(person) {
+function hasMother(person) {
+  return byName[person.mother] in ancestry;
+};
+//var ageDifference = hasMother.map(function(child) {
+function ageDifference(child) {
+  var mother = byName[child.mother];
+  return child.born - mother.born;
+};
+console.log(average(ancestry
+    .filter(hasMother)
+    .map(ageDifference)));
