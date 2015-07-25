f1cfd5fba51b747a41d7f5e054b16b55f55e47b3
skylurker <skaldsandshadows@mail.com>
2015-07-25 13:04:58 +0300

    EloqJS: Higher Order Functions, Flattening

--- EloquentJS/js/5_HigherOrderFunctions.js
+++ EloquentJS/js/5_HigherOrderFunctions.js
@@ -1,1 +1,10 @@
- 
+/* Eloquent Java Script */
+/* Higher Order Functions */
+
+/* Flattening */
+var input = [[1, 2, 3],
+	     [4, 5],
+	     [6, 7, 8, 9]];
+console.log(input.reduce(function (arrayA, arrayB) {
+			  return arrayA.concat(arrayB);
+}));
