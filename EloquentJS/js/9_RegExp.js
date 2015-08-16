d51fabc3c07ab0c2b2058b29c16aff7e76107728
skylurker <skaldsandshadows@mail.com>
2015-08-16 22:17:45 +0300

    EloqJS: ch9 RegExp, Blockquotes. It works, though there should be a more elegant solution

--- EloquentJS/js/9_RegExp.js
+++ EloquentJS/js/9_RegExp.js
@@ -13,3 +13,19 @@ console.log(re2.test("abxde"));
 console.log("папа".replace("п", "м"));
 
 console.log("Belekoroz".replace(/[eo]/g, "ё"));
+
+console.log(/\bcat\b/.test("cat\' lala"));
+console.log(/\bcat\b/.test("concatenate"));
+
+/* Одинарные кавычки заменить на двойные, но оставить апострофы */
+
+var text = "The words of the banished King, \'I swear REVENGE!\'... Full o\'hate, full o\'pride, oh, we screamed for revenge! \'Nightfall\' quietly it creptinan'changedusall";
+
+console.log("Blockquotes");
+console.log(text);
+//console.log(text.replace (/(\W)\'/g, "$1\""));
+//console.log(text.replace(/\'(\W)/g, "\"$1"));
+
+console.log(text.replace (/\W\'|\'\W/g, function(str){
+  return str.replace("\'", "\"");
+}));
