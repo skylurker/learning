afd06d7fbb6ac5b4ea2254dec62dc3fe5303d425
skylurker <skaldsandshadows@mail.com>
2016-09-29 00:57:36 +0400

    DP Drill Baby Drill, Divisible by 7, one-liner, JS

--- dailyprogrammer/Drill_Baby_Drill.js
+++ dailyprogrammer/Drill_Baby_Drill.js
@@ -11,3 +11,5 @@ function divisibleBy7(begin, end){
 }
 console.log(divisibleBy7(1, 1000));
 
+// http://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n#comment63637747_33352604
+console.log(Array.from(Array(1000)).map((e,i)=>i+1).filter(x => x % 7 == 0));
