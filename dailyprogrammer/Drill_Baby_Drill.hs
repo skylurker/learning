5c6e1e84ab8a7113c357e5ac1e5754fd0139a429
skylurker <skaldsandshadows@mail.com>
2016-09-29 00:44:11 +0400

    DP Drill Baby Drill, Number of spaces, Haskell

--- dailyprogrammer/Drill_Baby_Drill.hs
+++ dailyprogrammer/Drill_Baby_Drill.hs
@@ -1,3 +1,6 @@
+-- This file contains several programs for the same challenge entry (since there are sub-challenges).
+-- These mini-programs should be executed separately.
+
 -- September 08-09, 2016
 
 
@@ -8,3 +11,16 @@
 divisible = [ x | x <- [1..1000], x `mod` 7 == 0]
 
 main = mapM_ print divisible
+
+----------------------
+
+-- HASKELL
+-- https://www.reddit.com/r/dailyprogrammer_ideas/comments/4d5ehh/easy_drill_baby_drill/
+-- Count the number of spaces in a string
+
+-- spaces = length [ x | x <- "dsfj jhsdf jh jj dskj", x == ' ']
+spaces str = length [ x | x <- str, x == ' ']
+
+main = do
+    input <- getLine
+    print $ spaces input
