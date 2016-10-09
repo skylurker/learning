aa61bc6ceafff03967db7ad1aeed87db712a7e86
skylurker <skaldsandshadows@mail.com>
2016-10-09 18:45:53 +0400

    DP 286 Reverse Factorial, fixed, BASH

--- dailyprogrammer/286_Reverse_Factorial.sh
+++ dailyprogrammer/286_Reverse_Factorial.sh
@@ -2,20 +2,28 @@
 #https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/
 # [2016-10-03] Challenge #286 [Easy] Reverse Factorial
 # Oct 4th 2016
+
+# Here's the fixed version
+# Used this comment as a ref
+# https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/d8ca2ns
 function reverseFact {
-        INPUT=$1
+    INPUT=$1
     RESULT="$1"
     CNT=1
-    while [ $INPUT -gt 1 ]; do
-            INPUT=$(($INPUT/$CNT))
+    while [ $(($INPUT % $CNT)) -eq 0 ]; do
+        INPUT=$(($INPUT/$CNT))
         CNT=$(($CNT+1))
-        echo $INPUT
-        echo $CNT
+        #echo $INPUT
+        #echo $CNT
     done
-    CNT=$(($CNT-1))
-    echo "$RESULT = ${CNT}!"
+    if [ $(($CNT % $INPUT)) != 0 ]; then
+        echo "$RESULT NONE"
+    else
+        CNT=$(($CNT-1))
+        echo "$RESULT = ${CNT}!"
+    fi
 }
+reverseFact 3628800
+reverseFact 479001600
 reverseFact 6
-
-
-# DOESN'T HANDLE if it's not a factorial (the NONE case)
+reverseFact 18
