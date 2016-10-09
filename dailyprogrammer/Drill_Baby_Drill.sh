2bd6a648274416fb3f3ce0ac2d35fb3e7deaf075
skylurker <skaldsandshadows@mail.com>
2016-10-09 18:24:52 +0400

    DP Drill Baby Drill, have 3 in them, BASH

--- dailyprogrammer/Drill_Baby_Drill.sh
+++ dailyprogrammer/Drill_Baby_Drill.sh
@@ -14,3 +14,17 @@ while [ $COUNT -le 1000 ]; do
     fi
     COUNT=$((COUNT+1))
 done
+
+##########
+
+#!/bin/bash
+# https://www.reddit.com/r/dailyprogrammer_ideas/comments/4d5ehh/easy_drill_baby_drill/
+# Find all of the numbers from 1-1000 that have a 3 in them
+COUNT=1
+SUBSTRING=3
+until [ $COUNT -gt 1000 ]; do
+    if [ `expr index "$COUNT" "$SUBSTRING"` -gt 0 ]; then
+    	echo $COUNT
+    fi
+    COUNT=$((COUNT+1))
+done
