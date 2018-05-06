e36217abb33b12bca5f4d51174f28109c11a6a61
skylurker <skaldsandshadows@mail.com>
2018-05-07 00:13:13 +0400

    projecteuler 1 Multiples of 3 and 5, JS

---
+++ projecteuler/1_Multiples_of_3_and_5.js
@@ -0,0 +1,20 @@
+// Multiples of 3 and 5
+// https://projecteuler.net/problem=1
+// May 06, 2018
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+function sumOfMultiples(limit){
+	let sum = 0;
+	for (i = 1; i < limit; i++){
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	return sum;
+}
+
+console.log(sumOfMultiples(10));
+console.log(sumOfMultiples(1000));
